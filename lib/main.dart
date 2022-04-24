d96dc7218b5b63bcd95b31d3abe1c24b2d9e5b34
Diego Moraes <diegomoraes.a@gmail.com>
2022-04-24 11:38:21 -0300

    speedup 2

--- lib/main.dart
+++ lib/main.dart
@@ -47,8 +47,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
-                  children: const [
-                    Text(
+                  children: [
+                    const Text(
                       'Seu novo título',
                       style: TextStyle(
                         fontWeight: FontWeight.w500,
@@ -56,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         color: Color.fromRGBO(255, 255, 255, 0.8),
                       ),
                     ),
-                    Text(
+                    const Text(
                       'Nome do produto',
                       style: TextStyle(
                         fontWeight: FontWeight.w500,
@@ -65,7 +65,109 @@ class _MyHomePageState extends State<MyHomePage> {
                       ),
                     ),
                     // TODO: adicionar a Row com os containers azuis aqui
-                    SizedBox(
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 40),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Flexible(
+                            child: FittedBox(
+                              child: Container(
+                                constraints: const BoxConstraints(
+                                  maxWidth: 350,
+                                  maxHeight: 350,
+                                ),
+                                decoration: const BoxDecoration(
+                                  gradient: LinearGradient(
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight,
+                                    colors: [
+                                      Color.fromRGBO(82, 213, 255, 1),
+                                      Color.fromRGBO(165, 244, 255, 1),
+                                    ],
+                                  ),
+                                  borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 30),
+                          Flexible(
+                            child: Stack(
+                              alignment: AlignmentDirectional.center,
+                              children: [
+                                FittedBox(
+                                  child: Container(
+                                    constraints: const BoxConstraints(
+                                      maxWidth: 350,
+                                      maxHeight: 350,
+                                    ),
+                                    decoration: const BoxDecoration(
+                                      gradient: LinearGradient(
+                                        begin: Alignment.topLeft,
+                                        end: Alignment.bottomRight,
+                                        colors: [
+                                          Color.fromRGBO(82, 213, 255, 1),
+                                          Color.fromRGBO(165, 244, 255, 1),
+                                        ],
+                                      ),
+                                      borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                                    ),
+                                  ),
+                                ),
+                                Positioned(
+                                  bottom: 30,
+                                  child: Container(
+                                    width: 230,
+                                    height: 45,
+                                    decoration: const BoxDecoration(
+                                      color: Color.fromRGBO(165, 233, 255, 1),
+                                      borderRadius: BorderRadius.all(Radius.circular(53)),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Color.fromRGBO(0, 0, 0, 0.05),
+                                          blurRadius: 10.05,
+                                          offset: Offset(0.0, 3.44),
+                                        ),
+                                        BoxShadow(
+                                          color: Color.fromRGBO(0, 0, 0, 0.08),
+                                          blurRadius: 27.8,
+                                          offset: Offset(0.0, 9.52),
+                                        ),
+                                      ],
+                                    ),
+                                    child: const Center(child: Text('Saber mais')),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(width: 30),
+                          Flexible(
+                            child: FittedBox(
+                              child: Container(
+                                constraints: const BoxConstraints(
+                                  maxWidth: 350,
+                                  maxHeight: 350,
+                                ),
+                                decoration: const BoxDecoration(
+                                  gradient: LinearGradient(
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight,
+                                    colors: [
+                                      Color.fromRGBO(82, 213, 255, 1),
+                                      Color.fromRGBO(165, 244, 255, 1),
+                                    ],
+                                  ),
+                                  borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
                       width: 461,
                       child: Text(
                         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam gravida morbi diam consequat eget sit at a. '
