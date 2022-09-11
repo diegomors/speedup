28f17a2f2bff738d3f647ba8412477f765c0cb66
Ivaldo Cardoso Silva <ivaldoenilde@gmail.com>
2022-09-11 11:38:36 -0300

    correção das camada de opacidade do narrow, medium, wide.

--- lib/modules/product/widgets/my_product_background.dart
+++ lib/modules/product/widgets/my_product_background.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import '../../../shared/shared.dart';
 class MyProductBackground extends StatelessWidget {
   final Widget child;
 
@@ -15,12 +15,15 @@ class MyProductBackground extends StatelessWidget {
         Container(
           color: const Color.fromRGBO(0, 13, 37, 1),
         ),
+        if (context.screenSize == ScreenSize.medium || context.screenSize == ScreenSize.wide) ...[
         Container(
           color: const Color.fromRGBO(255, 255, 255, 0.05),
         ),
+        if (context.screenSize == ScreenSize.wide)
         Container(
           color: const Color.fromARGB(11, 136, 123, 123),
         ),
+      ],
         child,
       ],
     );
