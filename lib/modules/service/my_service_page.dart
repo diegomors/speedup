5ef08c391ed1b0d531272c5575f1af465c786692
Ivaldo Cardoso Silva <ivaldoenilde@gmail.com>
2022-11-17 11:14:45 -0300

    enviado para correção

--- lib/modules/service/my_service_page.dart
+++ lib/modules/service/my_service_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../../shared/shared.dart';
 
 class MyServicePage extends StatelessWidget {
@@ -8,13 +7,72 @@ class MyServicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: context.screenSize == ScreenSize.wide
-          ? Colors.amber
-          : context.screenSize == ScreenSize.medium
-              ? Colors.blue
-              : Colors.orange,
-      width: double.infinity,
-      height: 700,
+        color: const Color.fromRGBO(0, 13, 37, 1),
+        width: double.infinity,
+        height: 700,
+        child: Padding(
+            padding: const EdgeInsets.all(80.0),
+            child: context.screenSize == ScreenSize.narrow
+                ? PageView(
+                    controller: PageController(
+                      viewportFraction: 1 / 3.6,
+                      initialPage: 0,
+                    ),
+                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                    children: [
+                      buildContainer(context, addmargin: true),
+                      const SizedBox(
+                        width: 75,
+                      ),
+                      buildContainer(context, addmargin: true),
+                      const SizedBox(
+                        width: 75,
+                      ),
+                      buildContainer(context, addmargin: true),
+                      const SizedBox(
+                        width: 75,
+                      ),
+                      buildContainer(context, addmargin: true),
+                      const SizedBox(
+                        width: 75,
+                      ),
+                      buildContainer(context, addmargin: true),
+                    ],
+                  )
+                : Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      buildContainer(context, addmargin: true),
+                      const SizedBox(
+                        width: 75,
+                      ),
+                      buildContainer(context),
+                      const SizedBox(
+                        width: 75,
+                      ),
+                      buildContainer(context),
+                      const SizedBox(
+                        width: 75,
+                      ),
+                      buildContainer(context),
+                      const SizedBox(
+                        width: 75,
+                      ),
+                      buildContainer(context),
+                    ],
+                  )));
+  }
+
+  buildContainer(BuildContext context, {bool addmargin = false}) {
+    return Container(
+      width: context.screenSize == ScreenSize.narrow ? 72 : 132,
+      height: context.screenSize == ScreenSize.narrow ? 72 : 132,
+      constraints: const BoxConstraints(minHeight: 132, minWidth: 132),
+      decoration: const BoxDecoration(
+        color: Color.fromRGBO(161, 232, 255, 0.2),
+        shape: BoxShape.circle,
+      ),
     );
   }
 }
