79aa0d91c8e48e8641d8c42ad180227a89dc8ac2
Diego Moraes <diegomoraes.a@gmail.com>
2022-11-24 10:47:19 -0300

    fix overflow to show containers

--- lib/modules/service/my_service_page.dart
+++ lib/modules/service/my_service_page.dart
@@ -1,4 +1,3 @@
-import 'package:expandable_page_view/expandable_page_view.dart';
 import 'package:flutter/material.dart';
 
 import '../../shared/shared.dart';
@@ -8,62 +7,62 @@ class MyServicePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final containerSize = context.screenSize == ScreenSize.narrow ? 72.0 : 132.0;
+
     return Container(
-      color: context.screenSize == ScreenSize.wide
-          ? const Color.fromRGBO(0, 13, 37, 1)
-          : context.screenSize == ScreenSize.medium
-              ? const Color.fromRGBO(0, 13, 37, 1)
-              : Colors.orange,
+      color: const Color.fromRGBO(0, 13, 37, 1),
       width: double.infinity,
       height: 700,
       child: context.screenSize == ScreenSize.narrow
-          ? PageView(
-              controller: PageController(
-                viewportFraction: 1 / 3.6,
-                initialPage: 0,
-              ),
+          ? Column(
               children: [
-                buildContainer(context),
-                buildContainer(context),
-                buildContainer(context),
-                buildContainer(context),
-                buildContainer(context),
+                SizedBox(
+                  height: containerSize,
+                  child: PageView(
+                    controller: PageController(
+                      viewportFraction: 1 / 3.6,
+                      initialPage: 0,
+                    ),
+                    padEnds: false,
+                    children: [
+                      buildContainer(context, containerSize),
+                      buildContainer(context, containerSize),
+                      buildContainer(context, containerSize),
+                      buildContainer(context, containerSize),
+                      buildContainer(context, containerSize),
+                    ],
+                  ),
+                ),
               ],
             )
-          : PageView.builder(
-              controller: PageController(
-                viewportFraction: 1 / 5,
-                initialPage: 0,
-              ),
-              padEnds: false,
-              itemCount: 1,
-              itemBuilder: (context, index) => Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  buildContainer(context),
-                  buildContainer(context),
-                  buildContainer(context),
-                  buildContainer(context),
-                  buildContainer(context),
-                ],
-              ),
+          : Column(
+              children: [
+                SizedBox(
+                  width: 963,
+                  height: containerSize,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      buildContainer(context, containerSize),
+                      buildContainer(context, containerSize),
+                      buildContainer(context, containerSize),
+                      buildContainer(context, containerSize),
+                      buildContainer(context, containerSize),
+                    ],
+                  ),
+                ),
+              ],
             ),
     );
   }
 
-  buildContainer(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 37.5, right: 37.5),
-      child: Container(
-        width: context.screenSize == ScreenSize.narrow ? 72 : 132,
-        height: context.screenSize == ScreenSize.narrow ? 72 : 132,
-        // constraints: context.screenSize == ScreenSize.narrow
-        //     ? const BoxConstraints(minHeight: 72, minWidth: 72)
-        //     : const BoxConstraints(minHeight: 132, minWidth: 132),
-        decoration: const BoxDecoration(
-          color: Color.fromRGBO(161, 232, 255, 0.2),
-          shape: BoxShape.circle,
-        ),
+  buildContainer(BuildContext context, double containerSize) {
+    return Container(
+      width: context.screenSize == ScreenSize.narrow ? 72 : 132,
+      height: context.screenSize == ScreenSize.narrow ? 72 : 132,
+      decoration: const BoxDecoration(
+        color: Color.fromRGBO(161, 232, 255, 0.2),
+        shape: BoxShape.circle,
       ),
     );
   }
