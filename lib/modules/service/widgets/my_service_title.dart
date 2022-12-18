8038c4a1f29f1e3cdb797cabf14fb60159c8ee48
Ivaldo Cardoso Silva <ivaldoenilde@gmail.com>
2022-12-18 20:03:08 -0300

    refatoração mais espaçamento

---
+++ lib/modules/service/widgets/my_service_title.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../../../shared/components/my_text.dart';
+
+class MyServiceTitle extends StatelessWidget {
+  final String title;
+
+  const MyServiceTitle({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: const [
+        MyText(
+          'Serviços',
+          fontWeight: FontWeight.w500,
+          fontSize: 24,
+        ),
+        MyText(
+          'Política de serviço >',
+          fontWeight: FontWeight.w400,
+          fontSize: 16,
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/modules/service/widgets/widgets.dart
@@ -0,0 +1,2 @@
+export 'my_service_item.dart';
+export 'my_service_title.dart';
