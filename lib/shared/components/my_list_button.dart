2aac8d4d801f1b334af2724cd7e1527f565fa68e
Ivaldo Cardoso Silva <ivaldoenilde@gmail.com>
2023-04-10 22:18:54 -0300

    botão onPressed

--- lib/shared/components/my_list_button.dart
+++ lib/shared/components/my_list_button.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 
 class MyListButton extends StatelessWidget {
@@ -8,14 +10,39 @@ class MyListButton extends StatelessWidget {
     return buildRowArea();
   }
 
-  Widget buildRowArea() =>
-      Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        buildButton('Back-end'),
-        buildButton('Front-end'),
-        buildButton('Mobile')
-      ]);
+  Widget buildRowArea() => Wrap(children: [
+        //OutlinedButton(onPressed: () {}, child: const Text('Back-end')),
+        OutlinedButton(
+          onPressed: null,
+          style: OutlinedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(18.0),
+            ),
+            side: const BorderSide(
+                width: 1.0, color: Color.fromRGBO(255, 255, 255, 0.7)),
+          ),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 6.0,
+            ),
+            child: Text(
+              'Front-end',
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
 
-  Widget buildButton(String text) {
-    return Text(text);
-  }
+          /* buildButton(''),
+    buildButton('Front-end'),
+    buildButton('Mobile'),
+    buildButton('Flutter'),
+    buildButton('Java'),
+    buildButton('MVVM'),
+    buildButton('node.JS'),*/
+        ),
+      ]);
 }
